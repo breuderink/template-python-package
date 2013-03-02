3631998b077511966d8566b30340b1d89b77b2e9
Boris Reuderink <b.reuderink@gmail.com>
2013-03-02 13:52:37 +0100

    Boilerplate from The Hitchhiker’s Guide to Packaging.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name='MyPackage',
+  version='0.1.0',
+  author='B. Reuderink',
+  author_email='b.reuderink@gmail.com',
+  packages=['towelstuff', 'towelstuff.test'],
+  scripts=['bin/example_script'],
+  url='http://github.com/breuderink',
+  license='LICENSE.txt',
+  description='Useful towel-related stuff.',
+  long_description=open('README.txt').read(),
+  install_requires=[
+    "numpy>=1.6.2",
+  ],
+)
